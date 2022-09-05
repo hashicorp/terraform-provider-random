a59f8573088f20fda9894d1f38815c39e674a81c
Benjamin Bennett <ben.bennett@hashicorp.com>
2022-09-05 13:53:12 +0100

    Adding state upgrader to prevent password resource that has been imported from forcing a destroy-create on next terraform apply (#306)

--- internal/provider/resource_password.go
+++ internal/provider/resource_password.go
@@ -22,7 +22,7 @@ var _ provider.ResourceType = (*passwordResourceType)(nil)
 type passwordResourceType struct{}
 
 func (r *passwordResourceType) GetSchema(context.Context) (tfsdk.Schema, diag.Diagnostics) {
-	return passwordSchemaV2(), nil
+	return passwordSchemaV3(), nil
 }
 
 func (r *passwordResourceType) NewResource(_ context.Context, _ provider.Provider) (resource.Resource, diag.Diagnostics) {
@@ -38,7 +38,7 @@ var (
 type passwordResource struct{}
 
 func (r *passwordResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var plan passwordModelV2
+	var plan passwordModelV3
 
 	diags := req.Plan.Get(ctx, &plan)
 	resp.Diagnostics.Append(diags...)
@@ -65,7 +65,7 @@ func (r *passwordResource) Create(ctx context.Context, req resource.CreateReques
 		return
 	}
 
-	state := passwordModelV2{
+	state := passwordModelV3{
 		ID:              types.String{Value: "none"},
 		Keepers:         plan.Keepers,
 		Length:          types.Int64{Value: plan.Length.Value},
@@ -102,7 +102,7 @@ func (r *passwordResource) Read(ctx context.Context, req resource.ReadRequest, r
 
 // Update ensures the plan value is copied to the state to complete the update.
 func (r *passwordResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var model passwordModelV2
+	var model passwordModelV3
 
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &model)...)
 
@@ -121,7 +121,7 @@ func (r *passwordResource) Delete(ctx context.Context, req resource.DeleteReques
 func (r *passwordResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
 	id := req.ID
 
-	state := passwordModelV2{
+	state := passwordModelV3{
 		ID:              types.String{Value: "none"},
 		Result:          types.String{Value: id},
 		Length:          types.Int64{Value: int64(len(id))},
@@ -157,20 +157,25 @@ func (r *passwordResource) ImportState(ctx context.Context, req resource.ImportS
 func (r *passwordResource) UpgradeState(context.Context) map[int64]resource.StateUpgrader {
 	schemaV0 := passwordSchemaV0()
 	schemaV1 := passwordSchemaV1()
+	schemaV2 := passwordSchemaV2()
 
 	return map[int64]resource.StateUpgrader{
 		0: {
 			PriorSchema:   &schemaV0,
-			StateUpgrader: upgradePasswordStateV0toV2,
+			StateUpgrader: upgradePasswordStateV0toV3,
 		},
 		1: {
 			PriorSchema:   &schemaV1,
-			StateUpgrader: upgradePasswordStateV1toV2,
+			StateUpgrader: upgradePasswordStateV1toV3,
+		},
+		2: {
+			PriorSchema:   &schemaV2,
+			StateUpgrader: upgradePasswordStateV2toV3,
 		},
 	}
 }
 
-func upgradePasswordStateV0toV2(ctx context.Context, req resource.UpgradeStateRequest, resp *resource.UpgradeStateResponse) {
+func upgradePasswordStateV0toV3(ctx context.Context, req resource.UpgradeStateRequest, resp *resource.UpgradeStateResponse) {
 	type modelV0 struct {
 		ID              types.String `tfsdk:"id"`
 		Keepers         types.Map    `tfsdk:"keepers"`
@@ -194,8 +199,20 @@ func upgradePasswordStateV0toV2(ctx context.Context, req resource.UpgradeStateRe
 		return
 	}
 
-	passwordDataV2 := passwordModelV2{
-		Keepers:         passwordDataV0.Keepers,
+	keepers := types.Map{Null: true, ElemType: types.StringType}
+
+	if len(passwordDataV0.Keepers.Elems) > 0 {
+		keepers = passwordDataV0.Keepers
+	}
+
+	overrideSpecial := types.String{Null: true}
+
+	if len(passwordDataV0.OverrideSpecial.Value) > 0 {
+		overrideSpecial = passwordDataV0.OverrideSpecial
+	}
+
+	passwordDataV3 := passwordModelV3{
+		Keepers:         keepers,
 		Length:          passwordDataV0.Length,
 		Special:         passwordDataV0.Special,
 		Upper:           passwordDataV0.Upper,
@@ -206,24 +223,24 @@ func upgradePasswordStateV0toV2(ctx context.Context, req resource.UpgradeStateRe
 		MinUpper:        passwordDataV0.MinUpper,
 		MinLower:        passwordDataV0.MinLower,
 		MinSpecial:      passwordDataV0.MinSpecial,
-		OverrideSpecial: passwordDataV0.OverrideSpecial,
+		OverrideSpecial: overrideSpecial,
 		Result:          passwordDataV0.Result,
 		ID:              passwordDataV0.ID,
 	}
 
-	hash, err := generateHash(passwordDataV2.Result.Value)
+	hash, err := generateHash(passwordDataV3.Result.Value)
 	if err != nil {
 		resp.Diagnostics.Append(diagnostics.HashGenerationError(err.Error())...)
 		return
 	}
 
-	passwordDataV2.BcryptHash.Value = hash
+	passwordDataV3.BcryptHash.Value = hash
 
-	diags := resp.State.Set(ctx, passwordDataV2)
+	diags := resp.State.Set(ctx, passwordDataV3)
 	resp.Diagnostics.Append(diags...)
 }
 
-func upgradePasswordStateV1toV2(ctx context.Context, req resource.UpgradeStateRequest, resp *resource.UpgradeStateResponse) {
+func upgradePasswordStateV1toV3(ctx context.Context, req resource.UpgradeStateRequest, resp *resource.UpgradeStateResponse) {
 	type modelV1 struct {
 		ID              types.String `tfsdk:"id"`
 		Keepers         types.Map    `tfsdk:"keepers"`
@@ -248,8 +265,20 @@ func upgradePasswordStateV1toV2(ctx context.Context, req resource.UpgradeStateRe
 		return
 	}
 
-	passwordDataV2 := passwordModelV2{
-		Keepers:         passwordDataV1.Keepers,
+	keepers := types.Map{Null: true, ElemType: types.StringType}
+
+	if len(passwordDataV1.Keepers.Elems) > 0 {
+		keepers = passwordDataV1.Keepers
+	}
+
+	overrideSpecial := types.String{Null: true}
+
+	if len(passwordDataV1.OverrideSpecial.Value) > 0 {
+		overrideSpecial = passwordDataV1.OverrideSpecial
+	}
+
+	passwordDataV3 := passwordModelV3{
+		Keepers:         keepers,
 		Length:          passwordDataV1.Length,
 		Special:         passwordDataV1.Special,
 		Upper:           passwordDataV1.Upper,
@@ -260,13 +289,73 @@ func upgradePasswordStateV1toV2(ctx context.Context, req resource.UpgradeStateRe
 		MinUpper:        passwordDataV1.MinUpper,
 		MinLower:        passwordDataV1.MinLower,
 		MinSpecial:      passwordDataV1.MinSpecial,
-		OverrideSpecial: passwordDataV1.OverrideSpecial,
+		OverrideSpecial: overrideSpecial,
 		BcryptHash:      passwordDataV1.BcryptHash,
 		Result:          passwordDataV1.Result,
 		ID:              passwordDataV1.ID,
 	}
 
-	diags := resp.State.Set(ctx, passwordDataV2)
+	diags := resp.State.Set(ctx, passwordDataV3)
+	resp.Diagnostics.Append(diags...)
+}
+
+func upgradePasswordStateV2toV3(ctx context.Context, req resource.UpgradeStateRequest, resp *resource.UpgradeStateResponse) {
+	type modelV2 struct {
+		ID              types.String `tfsdk:"id"`
+		Keepers         types.Map    `tfsdk:"keepers"`
+		Length          types.Int64  `tfsdk:"length"`
+		Special         types.Bool   `tfsdk:"special"`
+		Upper           types.Bool   `tfsdk:"upper"`
+		Lower           types.Bool   `tfsdk:"lower"`
+		Number          types.Bool   `tfsdk:"number"`
+		Numeric         types.Bool   `tfsdk:"numeric"`
+		MinNumeric      types.Int64  `tfsdk:"min_numeric"`
+		MinUpper        types.Int64  `tfsdk:"min_upper"`
+		MinLower        types.Int64  `tfsdk:"min_lower"`
+		MinSpecial      types.Int64  `tfsdk:"min_special"`
+		OverrideSpecial types.String `tfsdk:"override_special"`
+		Result          types.String `tfsdk:"result"`
+		BcryptHash      types.String `tfsdk:"bcrypt_hash"`
+	}
+
+	var passwordDataV2 modelV2
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &passwordDataV2)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	keepers := types.Map{Null: true, ElemType: types.StringType}
+
+	if len(passwordDataV2.Keepers.Elems) > 0 {
+		keepers = passwordDataV2.Keepers
+	}
+
+	overrideSpecial := types.String{Null: true}
+
+	if len(passwordDataV2.OverrideSpecial.Value) > 0 {
+		overrideSpecial = passwordDataV2.OverrideSpecial
+	}
+
+	passwordDataV3 := passwordModelV3{
+		Keepers:         keepers,
+		Length:          passwordDataV2.Length,
+		Special:         passwordDataV2.Special,
+		Upper:           passwordDataV2.Upper,
+		Lower:           passwordDataV2.Lower,
+		Number:          passwordDataV2.Number,
+		Numeric:         passwordDataV2.Numeric,
+		MinNumeric:      passwordDataV2.MinNumeric,
+		MinUpper:        passwordDataV2.MinUpper,
+		MinLower:        passwordDataV2.MinLower,
+		MinSpecial:      passwordDataV2.MinSpecial,
+		OverrideSpecial: overrideSpecial,
+		BcryptHash:      passwordDataV2.BcryptHash,
+		Result:          passwordDataV2.Result,
+		ID:              passwordDataV2.ID,
+	}
+
+	diags := resp.State.Set(ctx, passwordDataV3)
 	resp.Diagnostics.Append(diags...)
 }
 
@@ -274,6 +363,190 @@ func generateHash(toHash string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(toHash), bcrypt.DefaultCost)
 
 	return string(hash), err
+}
+
+func passwordSchemaV3() tfsdk.Schema {
+	return tfsdk.Schema{
+		Version: 3,
+		Description: "Identical to [random_string](string.html) with the exception that the result is " +
+			"treated as sensitive and, thus, _not_ displayed in console output. Read more about sensitive " +
+			"data handling in the " +
+			"[Terraform documentation](https://www.terraform.io/docs/language/state/sensitive-data.html).\n\n" +
+			"This resource *does* use a cryptographic random number generator.",
+		Attributes: map[string]tfsdk.Attribute{
+			"keepers": {
+				Description: "Arbitrary map of values that, when changed, will trigger recreation of " +
+					"resource. See [the main provider documentation](../index.html) for more information.",
+				Type: types.MapType{
+					ElemType: types.StringType,
+				},
+				Optional: true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					planmodifiers.RequiresReplaceIfValuesNotNull(),
+				},
+			},
+
+			"length": {
+				Description: "The length of the string desired. The minimum value for length is 1 and, length " +
+					"must also be >= (`min_upper` + `min_lower` + `min_numeric` + `min_special`).",
+				Type:     types.Int64Type,
+				Required: true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					resource.RequiresReplace(),
+				},
+				Validators: []tfsdk.AttributeValidator{
+					int64validator.AtLeast(1),
+					int64validator.AtLeastSumOf(
+						path.MatchRoot("min_upper"),
+						path.MatchRoot("min_lower"),
+						path.MatchRoot("min_numeric"),
+						path.MatchRoot("min_special"),
+					),
+				},
+			},
+
+			"special": {
+				Description: "Include special characters in the result. These are `!@#$%&*()-_=+[]{}<>:?`. Default value is `true`.",
+				Type:        types.BoolType,
+				Optional:    true,
+				Computed:    true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					planmodifiers.DefaultValue(types.Bool{Value: true}),
+					planmodifiers.RequiresReplace(),
+				},
+			},
+
+			"upper": {
+				Description: "Include uppercase alphabet characters in the result. Default value is `true`.",
+				Type:        types.BoolType,
+				Optional:    true,
+				Computed:    true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					planmodifiers.DefaultValue(types.Bool{Value: true}),
+					planmodifiers.RequiresReplace(),
+				},
+			},
+
+			"lower": {
+				Description: "Include lowercase alphabet characters in the result. Default value is `true`.",
+				Type:        types.BoolType,
+				Optional:    true,
+				Computed:    true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					planmodifiers.DefaultValue(types.Bool{Value: true}),
+					planmodifiers.RequiresReplace(),
+				},
+			},
+
+			"number": {
+				Description: "Include numeric characters in the result. Default value is `true`. " +
+					"**NOTE**: This is deprecated, use `numeric` instead.",
+				Type:     types.BoolType,
+				Optional: true,
+				Computed: true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					planmodifiers.NumberNumericAttributePlanModifier(),
+					planmodifiers.RequiresReplace(),
+				},
+				DeprecationMessage: "**NOTE**: This is deprecated, use `numeric` instead.",
+			},
+
+			"numeric": {
+				Description: "Include numeric characters in the result. Default value is `true`.",
+				Type:        types.BoolType,
+				Optional:    true,
+				Computed:    true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					planmodifiers.NumberNumericAttributePlanModifier(),
+					planmodifiers.RequiresReplace(),
+				},
+			},
+
+			"min_numeric": {
+				Description: "Minimum number of numeric characters in the result. Default value is `0`.",
+				Type:        types.Int64Type,
+				Optional:    true,
+				Computed:    true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					planmodifiers.DefaultValue(types.Int64{Value: 0}),
+					planmodifiers.RequiresReplace(),
+				},
+			},
+
+			"min_upper": {
+				Description: "Minimum number of uppercase alphabet characters in the result. Default value is `0`.",
+				Type:        types.Int64Type,
+				Optional:    true,
+				Computed:    true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					planmodifiers.DefaultValue(types.Int64{Value: 0}),
+					planmodifiers.RequiresReplace(),
+				},
+			},
+
+			"min_lower": {
+				Description: "Minimum number of lowercase alphabet characters in the result. Default value is `0`.",
+				Type:        types.Int64Type,
+				Optional:    true,
+				Computed:    true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					planmodifiers.DefaultValue(types.Int64{Value: 0}),
+					planmodifiers.RequiresReplace(),
+				},
+			},
+
+			"min_special": {
+				Description: "Minimum number of special characters in the result. Default value is `0`.",
+				Type:        types.Int64Type,
+				Optional:    true,
+				Computed:    true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					planmodifiers.DefaultValue(types.Int64{Value: 0}),
+					planmodifiers.RequiresReplace(),
+				},
+			},
+
+			"override_special": {
+				Description: "Supply your own list of special characters to use for string generation.  This " +
+					"overrides the default character list in the special argument.  The `special` argument must " +
+					"still be set to true for any overwritten characters to be used in generation.",
+				Type:     types.StringType,
+				Optional: true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					resource.RequiresReplace(),
+				},
+			},
+
+			"result": {
+				Description: "The generated random string.",
+				Type:        types.StringType,
+				Computed:    true,
+				Sensitive:   true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					resource.UseStateForUnknown(),
+				},
+			},
+
+			"bcrypt_hash": {
+				Description: "A bcrypt hash of the generated random string.",
+				Type:        types.StringType,
+				Computed:    true,
+				Sensitive:   true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					resource.UseStateForUnknown(),
+				},
+			},
+
+			"id": {
+				Description: "A static value used internally by Terraform, this should not be referenced in configurations.",
+				Computed:    true,
+				Type:        types.StringType,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					resource.UseStateForUnknown(),
+				},
+			},
+		},
+	}
 }
 
 func passwordSchemaV2() tfsdk.Schema {
@@ -772,7 +1045,7 @@ func passwordSchemaV0() tfsdk.Schema {
 	}
 }
 
-type passwordModelV2 struct {
+type passwordModelV3 struct {
 	ID              types.String `tfsdk:"id"`
 	Keepers         types.Map    `tfsdk:"keepers"`
 	Length          types.Int64  `tfsdk:"length"`
--- internal/provider/resource_password_test.go
+++ internal/provider/resource_password_test.go
@@ -92,7 +92,7 @@ func TestAccResourcePassword_Import_332(t *testing.T) {
 				},
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"bcrypt_hash"},
+				ImportStateVerifyIgnore: []string{"bcrypt_hash", "override_special"},
 			},
 			{
 				ProtoV5ProviderFactories: protoV5ProviderFactories(),
@@ -863,9 +863,9 @@ func TestUpgradePasswordStateV0toV2(t *testing.T) {
 		},
 	}
 
-	upgradePasswordStateV0toV2(context.Background(), req, resp)
+	upgradePasswordStateV0toV3(context.Background(), req, resp)
 
-	expected := passwordModelV2{
+	expected := passwordModelV3{
 		ID:              types.String{Value: "none"},
 		Keepers:         types.Map{Null: true, ElemType: types.StringType},
 		Length:          types.Int64{Value: 16},
@@ -882,7 +882,7 @@ func TestUpgradePasswordStateV0toV2(t *testing.T) {
 		Result:          types.String{Value: "DZy_3*tnonj%Q%Yx"},
 	}
 
-	actual := passwordModelV2{}
+	actual := passwordModelV3{}
 	diags := resp.State.Get(context.Background(), &actual)
 	if diags.HasError() {
 		t.Errorf("error getting state: %v", diags)
@@ -932,9 +932,9 @@ func TestUpgradePasswordStateV1toV2(t *testing.T) {
 		},
 	}
 
-	upgradePasswordStateV1toV2(context.Background(), req, resp)
+	upgradePasswordStateV1toV3(context.Background(), req, resp)
 
-	expected := passwordModelV2{
+	expected := passwordModelV3{
 		ID:              types.String{Value: "none"},
 		Keepers:         types.Map{Null: true, ElemType: types.StringType},
 		Length:          types.Int64{Value: 16},
@@ -952,7 +952,70 @@ func TestUpgradePasswordStateV1toV2(t *testing.T) {
 		Result:          types.String{Value: "DZy_3*tnonj%Q%Yx"},
 	}
 
-	actual := passwordModelV2{}
+	actual := passwordModelV3{}
+	diags := resp.State.Get(context.Background(), &actual)
+	if diags.HasError() {
+		t.Errorf("error getting state: %v", diags)
+	}
+
+	if !cmp.Equal(expected, actual) {
+		t.Errorf("expected: %+v, got: %+v", expected, actual)
+	}
+}
+
+func TestUpgradePasswordStateV2toV3(t *testing.T) {
+	raw := tftypes.NewValue(tftypes.Object{}, map[string]tftypes.Value{
+		"id":               tftypes.NewValue(tftypes.String, "none"),
+		"keepers":          tftypes.NewValue(tftypes.Map{ElementType: tftypes.String}, nil),
+		"length":           tftypes.NewValue(tftypes.Number, 16),
+		"lower":            tftypes.NewValue(tftypes.Bool, true),
+		"min_lower":        tftypes.NewValue(tftypes.Number, 0),
+		"min_numeric":      tftypes.NewValue(tftypes.Number, 0),
+		"min_special":      tftypes.NewValue(tftypes.Number, 0),
+		"min_upper":        tftypes.NewValue(tftypes.Number, 0),
+		"number":           tftypes.NewValue(tftypes.Bool, true),
+		"numeric":          tftypes.NewValue(tftypes.Bool, true),
+		"override_special": tftypes.NewValue(tftypes.String, "!#$%\u0026*()-_=+[]{}\u003c\u003e:?"),
+		"result":           tftypes.NewValue(tftypes.String, "DZy_3*tnonj%Q%Yx"),
+		"special":          tftypes.NewValue(tftypes.Bool, true),
+		"upper":            tftypes.NewValue(tftypes.Bool, true),
+		"bcrypt_hash":      tftypes.NewValue(tftypes.String, "bcrypt_hash"),
+	})
+
+	req := res.UpgradeStateRequest{
+		State: &tfsdk.State{
+			Raw:    raw,
+			Schema: passwordSchemaV2(),
+		},
+	}
+
+	resp := &res.UpgradeStateResponse{
+		State: tfsdk.State{
+			Schema: passwordSchemaV3(),
+		},
+	}
+
+	upgradePasswordStateV2toV3(context.Background(), req, resp)
+
+	expected := passwordModelV3{
+		ID:              types.String{Value: "none"},
+		Keepers:         types.Map{Null: true, ElemType: types.StringType},
+		Length:          types.Int64{Value: 16},
+		Special:         types.Bool{Value: true},
+		Upper:           types.Bool{Value: true},
+		Lower:           types.Bool{Value: true},
+		Number:          types.Bool{Value: true},
+		Numeric:         types.Bool{Value: true},
+		MinNumeric:      types.Int64{Value: 0},
+		MinUpper:        types.Int64{Value: 0},
+		MinLower:        types.Int64{Value: 0},
+		MinSpecial:      types.Int64{Value: 0},
+		OverrideSpecial: types.String{Value: "!#$%\u0026*()-_=+[]{}\u003c\u003e:?"},
+		BcryptHash:      types.String{Value: "bcrypt_hash"},
+		Result:          types.String{Value: "DZy_3*tnonj%Q%Yx"},
+	}
+
+	actual := passwordModelV3{}
 	diags := resp.State.Get(context.Background(), &actual)
 	if diags.HasError() {
 		t.Errorf("error getting state: %v", diags)
