c9f46f39f5c70d77d5e2bb92d71c57e51e75905f
Socolin <provost.bertrand@gmail.com>
2023-02-14 02:25:51 -0500

    Add tests for keepers of random_bytes

--- internal/provider/resource_bytes_test.go
+++ internal/provider/resource_bytes_test.go
@@ -91,3 +91,522 @@ func TestAccResourceBytes_Length_ForceReplacement(t *testing.T) {
 		},
 	})
 }
+
+func TestAccResourceBytes_Keepers_Keep_EmptyMap(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "0"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesEqual(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Keep_EmptyMapToNullValue(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "0"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = null
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesEqual(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Keep_NullMap(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "0"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesEqual(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Keep_NullMapToNullValue(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "0"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = null
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesEqual(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Keep_NullValue(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = null
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = null
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesEqual(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Keep_NullValues(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key1" = null
+						"key2" = null
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "2"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key1" = null
+						"key2" = null
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesEqual(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "2"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Keep_Value(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = "123"
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = "123"
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesEqual(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Keep_Values(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key1" = "123"
+						"key2" = "456"
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "2"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key1" = "123"
+						"key2" = "456"
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesEqual(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "2"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Replace_EmptyMapToValue(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "0"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = "123"
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesDiffer(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Replace_NullMapToValue(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "0"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = "123"
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesDiffer(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Replace_NullValueToValue(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = null
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = "123"
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesDiffer(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Replace_ValueToEmptyMap(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = "123"
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesDiffer(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Replace_ValueToNullMap(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = "123"
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesDiffer(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Replace_ValueToNullValue(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = "123"
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = null
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesDiffer(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceBytes_Keepers_Replace_ValueToNewValue(t *testing.T) {
+	var result1, result2 string
+
+	resource.ParallelTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = "123"
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result1),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+			{
+				ProtoV5ProviderFactories: protoV5ProviderFactories(),
+				Config: `resource "random_bytes" "test" {
+					length = 12
+					keepers = {
+						"key" = "456"
+					}
+				}`,
+				Check: resource.ComposeTestCheckFunc(
+					testExtractResourceAttr("random_bytes.test", "result_hex", &result2),
+					testCheckAttributeValuesDiffer(&result1, &result2),
+					resource.TestCheckResourceAttr("random_bytes.test", "keepers.%", "1"),
+				),
+			},
+		},
+	})
+}
