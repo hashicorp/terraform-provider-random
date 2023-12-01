8acd2b17a188514c4610bc3cd28a7bfbc84d5a8e
Benjamin Bennett <ben.bennett@hashicorp.com>
2023-12-01 11:23:42 +0000

    Using mapplanmodifier.RequiresReplace() in place of mapplanmodifiers.RequiresReplaceIfValuesNotNull()
      * There should be no instances of prior state for random_bytes as it was never present in the SDKv2 version of the random provider

--- internal/provider/resource_bytes.go
+++ internal/provider/resource_bytes.go
@@ -14,13 +14,13 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/mapplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	"github.com/terraform-providers/terraform-provider-random/internal/diagnostics"
-	mapplanmodifiers "github.com/terraform-providers/terraform-provider-random/internal/planmodifiers/map"
 )
 
 var (
@@ -143,11 +143,7 @@ func bytesSchemaV0() schema.Schema {
 				ElementType: types.StringType,
 				Optional:    true,
 				PlanModifiers: []planmodifier.Map{
-					// mapplanmodifiers.RequiresReplaceIfValuesNotNull() has been used for consistency with other
-					// resources but mapplanmodifier.RequiresReplace() could have been used as there shouldn't be any
-					// prior state storage from a terraform-plugin-sdk based resource which would've collapsed a map
-					// of null values into a null map.
-					mapplanmodifiers.RequiresReplaceIfValuesNotNull(),
+					mapplanmodifier.RequiresReplace(),
 				},
 			},
 			"length": schema.Int64Attribute{
