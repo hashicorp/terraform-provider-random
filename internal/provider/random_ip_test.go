1948ac776810254cb437d9481293967503059c73
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-01-21 18:23:00 +0100

    chore: add ipv6 test for `random_ip`

--- internal/provider/random_ip_test.go
+++ internal/provider/random_ip_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccResourceIP(t *testing.T) {
+func TestAccResourceIP_ipv4(t *testing.T) {
 	resource.UnitTest(t, resource.TestCase{
 		ProtoV5ProviderFactories: protoV5ProviderFactories(),
 		Steps: []resource.TestStep{
@@ -19,7 +19,31 @@ func TestAccResourceIP(t *testing.T) {
 							cidr_range   = "10.0.0.0/16"
 						}`,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("random_ip.example", "address_type"),
+					resource.TestCheckResourceAttr("random_ip.example", "address_type", "ipv4"),
+					resource.TestCheckResourceAttrSet("random_ip.example", "cidr_range"),
+					resource.TestCheckResourceAttrSet("random_ip.example", "result"),
+				),
+			},
+			{
+				ResourceName:      "random_ip.example",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccResourceIP_ipv6(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV5ProviderFactories: protoV5ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: `resource "random_ip" "basic" {
+							address_type = "ipv6"
+							cidr_range   = "2001:db8::/16"
+						}`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("random_ip.example", "address_type", "ipv6"),
 					resource.TestCheckResourceAttrSet("random_ip.example", "cidr_range"),
 					resource.TestCheckResourceAttrSet("random_ip.example", "result"),
 				),
